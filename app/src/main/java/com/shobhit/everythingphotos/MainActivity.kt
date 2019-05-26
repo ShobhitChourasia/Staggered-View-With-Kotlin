a169236ce5b56d9f0a90b0da7686b2ecbb09830f
Shobhit Chourasia <>
2019-05-26 21:54:30 +0530

    Initial commit containing OkHttp and Gson in gradle file. UI Contains splash screen. Contains response parsing for Pexel curated API.

---
+++ app/src/main/java/com/shobhit/everythingphotos/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.shobhit.everythingphotos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.GsonBuilder
+import com.google.gson.internal.GsonBuildConfig
+import okhttp3.*
+import java.io.IOException
+
+val authKey_Pexel = "563492ad6f917000010000010b539d05730741d9909e8c7769de4389"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fetchDataFromPexelServer()
+    }
+
+
+    fun fetchDataFromPexelServer() {
+
+        val pexelUrl = "https://api.pexels.com/v1/curated?per_page=15&page=1"
+
+        var request = Request.Builder().url(pexelUrl).build()
+
+        val client = OkHttpClient().newBuilder().addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val builder = originalRequest.newBuilder()
+                .header("Authorization", authKey_Pexel)
+            val newRequest = builder.build()
+            chain.proceed(newRequest)
+        }.build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println(call)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                val responseBody = response.body()?.string()
+//                println(response.body()?.string())
+
+                val gsonData = GsonBuilder().create()
+
+                val pexelData = gsonData.fromJson(responseBody, PexelPageData::class.java)
+            }
+        })
+    }
+}
+
+
+class PexelData(val pexelData: List<PexelPageData>)
+
+class PexelPageData (val page: Int, val per_page: Int, val next_page: String,
+                     val prev_page: String, val photos: List<PexelPhotos>)
+
+class PexelPhotos (val id: String, val width: Int, val height: Int, val url: String,
+                   val photographer: String, val photographer_url: String, val src: PhotoSource)
+
+class PhotoSource (val original: String, val large2x: String, val large: String, val medium: String,
+                   val small: String, val portrait: String, val landscape: String, val tiny: String)
